77b84528dc490d3ba05072f00dda73c7ae90db5b
Annie Ross <annieross10@outlook.com>
2023-05-23 14:53:33 +0100

    add question data in js

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+const questions = [
+    {
+      question: "What is the main ingredient used in the production of Scottish Whiskey?",
+      answers: [
+        { text: "Barley", correctAnswer: true },
+        { text: "Rye", correctAnswer: false },
+        { text: "Corn", correctAnswer: false },
+        { text: "Wheat", correctAnswer: false },
+      ],
+    },
+    {
+      question: "Which region of Scotland is known for producing whiskey with a distinctive smoky flavour?",
+      answers: [
+        { text: "Speyside", correctAnswer: false },
+        { text: "Highlands", correctAnswer: false },
+        { text: "Islay", correctAnswer: true },
+        { text: "Lowlands", correctAnswer: false },
+      ],
+    },
+    {
+      question: "Which well-known Scottish Whiskey brand uses a stag's head as its logo?",
+      answers: [
+        { text: "Talisker", correctAnswer: false },
+        { text: "Dalmore", correctAnswer: true },
+        { text: "Bowmore", correctAnswer: false },
+        { text: "Aberfeldy", correctAnswer: false },
+      ],
+    },
+    {
+      question: "Which Scottish whiskey is often called the The King of Malts and is known for its smooth, honeyed character? ",
+      answers: [
+        { text: "Talisker", correctAnswer: false },
+        { text: "Balvenie", correctAnswer: false },
+        { text: "Macallan", correctAnswer: false },
+        { text: "Glenlivet", correctAnswer: true },
+      ],
+    },
+    {
+      question: "Which Scottish whiskey is made exclusively from malted barley and produced in a single distillery? ",
+      answers: [
+        { text: "Glenmorangie", correctAnswer: true },
+        { text: "Johnnie Walker", correctAnswer: false },
+        { text: "Laphroig", correctAnswer: false },
+        { text: "Glenfiddoch", correctAnswer: false },
+      ],
+    },
+    {
+        question: "Which Scottish whiskey is known for its peaty and smoky flavor, often described as medicinal? ",
+        answers: [
+          { text: "Dalwhinnie", correctAnswer: false },
+          { text: "Glenlivet", correctAnswer: false },
+          { text: "Laphroaig", correctAnswer: true },
+          { text: "Glenfiddich", correctAnswer: false },
+        ],
+      },
+      {
+        question: "What is the traditional ingredient used in the production of Talisker whiskey that gives it a unique maritime character?  ",
+        answers: [
+          { text: "Brine", correctAnswer: true },
+          { text: "Seaweed", correctAnswer: false },
+          { text: "Peat", correctAnswer: false },
+          { text: "Heather", correctAnswer: false },
+        ],
+      },
+  ];
